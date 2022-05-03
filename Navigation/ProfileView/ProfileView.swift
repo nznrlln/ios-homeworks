2dfc973e481fd8d1df2a613f0b10bab81268e95d
nznrlln <102247982+nznrlln@users.noreply.github.com>
2022-05-03 20:26:06 +0300

    ProfileView added

---
+++ Navigation/ProfileView/ProfileView.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by Нияз Нуруллин on 03.05.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var profileAvatar: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profileDateOfBirth: UILabel!
+    @IBOutlet weak var profileCity: UILabel!
+    @IBOutlet weak var profileText: UITextView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
